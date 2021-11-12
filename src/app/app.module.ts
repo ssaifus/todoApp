32863d3fb7095bb52e6e56500c402b47b99b5528
Syed Saifus Zoha <31951383+ssaifus@users.noreply.github.com>
2021-11-13 00:13:16 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoDetailsComponent } from './todo-details/todo-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoListComponent,
+    TodoDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatCheckboxModule,
+    BrowserAnimationsModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
